c762df64cd5cd8479770a9ed8eae3e7f514a51b7
C.LUANA.PEIXOTO <c.luana.peixoto@dpaschoal.onmicrosoft.com>
2024-07-16 16:26:23 -0300

    ajustes

--- sakai-ng-master/src/app/demo/components/out/out.component.ts
+++ sakai-ng-master/src/app/demo/components/out/out.component.ts
@@ -1,8 +1,21 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   templateUrl: './out.component.html',
   styleUrl: './out.component.scss'
 })
-export class OutComponent {}
+export class OutComponent {
+
+  constructor(private router: Router) {}
+  ngOnInit() 
+  {
+       this.logout();
+
+  }
+  logout() {
+    localStorage.clear();
+    this.router.navigate(['auth/login']);
+  }
+}
 
